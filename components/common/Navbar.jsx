cce21696d1681c0fc5df4a3e03281d43d1c1320c
Suresh-8695198 <suresh169073@periyaruniversity.ac.in>
2026-03-16 09:11:53 +0530

    fix: enforce strict max-width on sidebar logo to prevent overlap on small screens

--- components/common/Navbar.jsx
+++ components/common/Navbar.jsx
@@ -843,10 +843,10 @@ const Navbar = () => {
                         >
                             {/* Drawer header */}
                             <div className={`flex items-center justify-between px-5 h-16 border-b ${isDark ? 'border-white/5' : 'border-gray-100'}`}>
-                                <Link href='/' onClick={() => setMobileOpen(false)} className='flex items-center min-w-0 flex-1 mr-4'>
+                                <Link href='/' onClick={() => setMobileOpen(false)} className='flex items-center min-w-0 flex-1 mr-6'>
                                     <div className="flex items-center gap-1 min-w-0">
                                         <img src='/logo.png' alt='Logo' className='h-8 w-auto object-contain shrink-0' />
-                                        <div className='overflow-hidden truncate hidden min-[360px]:block'>
+                                        <div className='overflow-hidden max-w-[140px] xs:max-w-[170px] hidden min-[340px]:block'>
                                             <AnimatedLogo size="sm" loopInterval={30000} className="whitespace-nowrap" />
                                         </div>
                                     </div>
